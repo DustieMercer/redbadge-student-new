742fa9008063dd12a6fc006de1ff23558bca2a41
Dustie Mercer <d.nichole.mercer@gmail.com>
2021-03-23 20:11:03 -0400

    props demo

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { visitEachChild } from "typescript";
+import PropsExample from '../src/components/PropsExample'
+import PropsMapping from "../src/components/PropsMapping";
 
 function App() {
-  return <div></div>;
+  const visitedPlaces = ['Mt. Arenal', 'Disney World', 'St. Johns', 'Puerto Vallarta', 'Sturgis', 'Waikiki' ]
+  return <div>
+    <PropsExample name="Tom" business="MySpace"/>
+    <PropsMapping visited={visitedPlaces}/>
+    </div>;
+  
 }
 
 export default App;
